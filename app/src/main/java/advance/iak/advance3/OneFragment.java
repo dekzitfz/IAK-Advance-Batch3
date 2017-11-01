94686e4cc4272648b980f963de205bfe2b9d54e8
Adi Andrea <dekz.itfz@gmail.com>
2017-11-01 13:14:10 +0800

    load fragment to activity

---
+++ app/src/main/java/advance/iak/advance3/OneFragment.java
@@ -0,0 +1,18 @@
+package advance.iak.advance3;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OneFragment extends Fragment {
+
+    public OneFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        return view;
+    }
+}
