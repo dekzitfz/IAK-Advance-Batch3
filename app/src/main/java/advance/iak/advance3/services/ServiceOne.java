f0aaf7bd6af7abdf5c9d63ffaea8de8cdd6c610f
Adi Andrea <dekz.itfz@gmail.com>
2017-11-01 23:36:22 +0800

    run service on background thread

--- app/src/main/java/advance/iak/advance3/services/ServiceOne.java
+++ app/src/main/java/advance/iak/advance3/services/ServiceOne.java
@@ -2,6 +2,7 @@ package advance.iak.advance3.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -13,12 +14,7 @@ public class ServiceOne extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(TAG, "onStartCommand called");
-        try {
-            Thread.sleep(10000);
-            Log.i(TAG, "10 sec has passed!");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        new DoSomething().execute();
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -27,5 +23,21 @@ public class ServiceOne extends Service {
     public IBinder onBind(Intent intent) {
         Log.i(TAG, "onBind called");
         return null;
+    }
+
+    private static class DoSomething extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            for(int  i=0; i<=10; i++){
+                try {
+                    Thread.sleep(1000);
+                    Log.i(TAG, "doInBackground -> "+i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
     }
 }
