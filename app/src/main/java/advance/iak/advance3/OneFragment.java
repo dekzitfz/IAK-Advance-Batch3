2c070dbcefef3f744c5a99ca5debbb35c2aeed27
Adi Andrea <dekz.itfz@gmail.com>
2017-11-01 13:25:24 +0800

    add/replace with fragment two

--- app/src/main/java/advance/iak/advance3/OneFragment.java
+++ app/src/main/java/advance/iak/advance3/OneFragment.java
@@ -5,14 +5,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class OneFragment extends Fragment {
+public class OneFragment extends Fragment implements View.OnClickListener {
 
     public OneFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_one, container, false);
+
+        Button btn = view.findViewById(R.id.btn_goto_fragment_two);
+        btn.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_goto_fragment_two:
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, new TwoFragment(), null)
+                        .commit();
+                break;
+            default:
+        }
     }
 }
