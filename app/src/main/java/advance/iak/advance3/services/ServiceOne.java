d1f3b1b0ba1341f560a61c1f207629545fb65c1e
Adi Andrea <dekz.itfz@gmail.com>
2017-11-02 22:32:45 +0800

    bind service to activity

--- app/src/main/java/advance/iak/advance3/services/ServiceOne.java
+++ app/src/main/java/advance/iak/advance3/services/ServiceOne.java
@@ -3,6 +3,8 @@ package advance.iak.advance3.services;
 import android.app.Service;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -10,34 +12,37 @@ import android.util.Log;
 public class ServiceOne extends Service {
 
     private static final String TAG = ServiceOne.class.getSimpleName();
+    private final IBinder mBinder = new ServiceOneBinder();
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(TAG, "onStartCommand called");
-        new DoSomething().execute();
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        Log.i(TAG, "onBind called");
-        return null;
+        Log.i(TAG, "onBind");
+        return mBinder;
     }
 
-    private static class DoSomething extends AsyncTask<Void, Void, Void> {
+    @Override
+    public boolean onUnbind(Intent intent) {
+        //cancel your request / task
+        Log.i(TAG, "onUnbind");
+        return super.onUnbind(intent);
+    }
 
-        @Override
-        protected Void doInBackground(Void... voids) {
-            for(int  i=0; i<=10; i++){
-                try {
-                    Thread.sleep(1000);
-                    Log.i(TAG, "doInBackground -> "+i);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
+    public void startSomething(){
+        //do something
+        Log.i(TAG, "startSomething");
+    }
+
+    public class ServiceOneBinder extends Binder {
+        ServiceOne getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return ServiceOne.this;
         }
     }
 }
--- app/src/main/java/advance/iak/advance3/services/ServiceSampleActivity.java
+++ app/src/main/java/advance/iak/advance3/services/ServiceSampleActivity.java
@@ -1,27 +1,79 @@
 package advance.iak.advance3.services;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import advance.iak.advance3.R;
 
 public class ServiceSampleActivity extends AppCompatActivity {
+
+    private static final String TAG  = ServiceSampleActivity.class.getSimpleName();
+
+    private ServiceConnection serviceConnection;
+    private boolean serviceBounded = false;
+    ServiceOne serviceOne;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_sample);
 
+        serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                Log.i(TAG, "onServiceConnected");
+                // bounded to ServiceOne, cast the IBinder and get ServiceOne instance
+                ServiceOne.ServiceOneBinder binder = (ServiceOne.ServiceOneBinder) iBinder;
+                serviceOne = binder.getService();
+                serviceBounded = true;
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                Log.i(TAG, "onServiceDisconnected");
+                serviceBounded = false;
+            }
+        };
+
         Button btnStartService = findViewById(R.id.btn_start_service);
         btnStartService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startService(new Intent(ServiceSampleActivity.this, ServiceOne.class));
+                if(serviceBounded){
+                    serviceOne.startSomething();
+                }else{
+                    Toast.makeText(ServiceSampleActivity.this, "not bounded", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        bindService(
+                new Intent(ServiceSampleActivity.this, ServiceOne.class),
+                serviceConnection,
+                Context.BIND_AUTO_CREATE
+        );
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "onStop");
+        super.onStop();
+        unbindService(serviceConnection);
+        serviceBounded = false;
     }
 }
