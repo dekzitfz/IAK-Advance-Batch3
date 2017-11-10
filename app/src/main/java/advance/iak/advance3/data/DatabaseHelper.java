afc8d5214691670a2f678b2d38efcdd8a457213a
Adi Andrea <dekz.itfz@gmail.com>
2017-11-10 21:45:24 +0800

    create database helper

---
+++ app/src/main/java/advance/iak/advance3/data/DatabaseHelper.java
@@ -0,0 +1,25 @@
+package advance.iak.advance3.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "mydata";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
