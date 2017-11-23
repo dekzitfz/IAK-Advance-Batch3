81cacf25442af49072501650f8277477971568da
Adi Andrea <dekz.itfz@gmail.com>
2017-11-23 21:05:19 +0800

    change Call to Single

--- app/src/main/java/advance/iak/advance3/rest/GithubAPI.java
+++ app/src/main/java/advance/iak/advance3/rest/GithubAPI.java
@@ -1,12 +1,12 @@
 package advance.iak.advance3.rest;
 
 import advance.iak.advance3.model.User;
-import retrofit2.Call;
+import io.reactivex.Single;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface GithubAPI {
 
     @GET("users/{username}")
-    Call<User> getUser(@Path("username") String username);
+    Single<User> getUser(@Path("username") String username);
 }
