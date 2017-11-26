c889910a46d413d4f7b25c989ef5069471fc1304
Adi Andrea <dekz.itfz@gmail.com>
2017-11-26 10:00:58 +0800

    init widget

---
+++ app/src/main/java/advance/iak/advance3/widget/MyWidget.java
@@ -0,0 +1,51 @@
+package advance.iak.advance3.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import advance.iak.advance3.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class MyWidget extends AppWidgetProvider {
+
+    private static String TAG = MyWidget.class.getSimpleName();
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.my_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Log.d(TAG, "onUpdate");
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        Log.d(TAG, "onEnabled");
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        Log.d(TAG, "onDisabled");
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
