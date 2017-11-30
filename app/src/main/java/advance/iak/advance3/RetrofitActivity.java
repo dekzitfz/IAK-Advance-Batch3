cad960cc21f9dedbf7c7017bcd19ac34da7ca1bd
Adi Andrea <dekz.itfz@gmail.com>
2017-11-30 15:29:55 +0800

    setup idling resource state

--- app/src/main/java/advance/iak/advance3/RetrofitActivity.java
+++ app/src/main/java/advance/iak/advance3/RetrofitActivity.java
@@ -1,10 +1,14 @@
 package advance.iak.advance3;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.IdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import advance.iak.advance3.model.User;
@@ -20,20 +24,46 @@ public class RetrofitActivity extends AppCompatActivity {
 
     private GithubAPI service;
 
+    @Nullable
+    private GithubIdlingResource idlingResource;
+
+    @VisibleForTesting
+    @NonNull
+    public IdlingResource getIdlingResource(){
+        if(idlingResource == null){
+            idlingResource = new GithubIdlingResource();
+        }
+
+        return idlingResource;
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrofit);
 
+        TextView githubName = findViewById(R.id.github_name);
+
+        getIdlingResource();
         service = MyApp.getREST().getService();
 
         Button btnRetrofit = findViewById(R.id.btn_retrofit);
-        btnRetrofit.setOnClickListener(v -> service.getUser("dekzitfz")
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        user -> Toast.makeText(RetrofitActivity.this, user.getName(), Toast.LENGTH_SHORT).show(),
-                        error -> Toast.makeText(RetrofitActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show()
-                ));
+        btnRetrofit.setOnClickListener(v -> {
+                if(idlingResource!=null) idlingResource.setIdleState(false);
+                service.getUser("dekzitfz")
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            user -> {
+                                githubName.setText(user.getName());
+                                idlingResource.setIdleState(true);
+                                },
+                            error -> {
+                                Toast.makeText(RetrofitActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                                idlingResource.setIdleState(true);
+                            }
+                    );
+            }
+        );
     }
 }
